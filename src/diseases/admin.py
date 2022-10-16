82faacd3a56a14664ca0ee2683a750f9e0d3786e
Robert Levchenko <ro.le.00@mail.ru>
2022-10-16 17:56:03 +0300

    add Disease, Symptom and DiseaseHistory models to diseases.models.py

--- src/diseases/admin.py
+++ src/diseases/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from diseases.models import Disease
 
-# Register your models here.
+
+@admin.register(Disease)
+class DiseaseAdmin(admin.ModelAdmin):
+    list_display = ("title", "description")
--- src/diseases/models.py
+++ src/diseases/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 User = get_user_model()
 
+
 class Disease(models.Model):
     """Disease model, obviously."""
 
@@ -11,3 +12,53 @@ class Disease(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Symptom(models.Model):
+    """Single symptom, one of a kind."""
+
+    # SYSTEMS = {
+        # "N": "nervous system",
+        # "E": "endocrine system",
+        # another systems...
+    #}
+
+    # system = models.CharField(max_length=1, choices=SYSTEMS)
+    description = models.TextField
+
+    # class Meta:
+        # ordering = ["system"]
+
+    def __str__(self):
+        return self.description[:100]
+
+
+class DiseaseHistory:
+    """Disease-symptom-person relations."""
+
+    patient = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="diseases",
+        verbose_name="Больной",
+    )
+
+    disease = models.ForeignKey(
+        Disease,
+        on_delete=models.SET_NULL,
+        related_name="diseases",
+        verbose_name="Заболевание",
+    )
+
+    symptom = models.ManyToManyField(
+        Symptom,
+    )
+
+    first_symtoms = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Начало болезни",
+    )
+
+    recovered = models.DateTimeField(
+        verbose_name="Окончание болезни",
+    )
