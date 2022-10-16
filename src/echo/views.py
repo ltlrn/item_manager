8c9383aff20e1e91e5c5dd7c352f4b7b4654d3ae
Robert Levchenko <ro.le.00@mail.ru>
2022-10-16 20:37:03 +0300

    another changes

--- src/echo/views.py
+++ src/echo/views.py
@@ -1,5 +1,7 @@
 from rest_framework import status
-from rest_framework.authentication import TokenAuthentication, BasicAuthentication
+from rest_framework.authentication import (BasicAuthentication,
+                                           TokenAuthentication)
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,6 +10,7 @@ from echo.models import Echo
 
 class EchoView(APIView):
     authentication_classes = [TokenAuthentication, BasicAuthentication]
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         if request.user.is_anonymous:
