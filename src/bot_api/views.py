8fd83efd60dba1a12e1eff1b24e3c5f3981d54fa
Robert Levchenko <ro.le.00@mail.ru>
2023-01-22 00:07:50 +0300

    add action dec into view

--- src/bot_api/views.py
+++ src/bot_api/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -9,3 +11,10 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     permission_classes = [AllowAny]
     serializer_class = TaskSerializer
+
+    @action(detail=False, url_path="give-image")
+    def give_image(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True, context={"request": request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
